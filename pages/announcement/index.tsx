fba07a8049bd82d8946d631b0043d25960565fff
k1105 <kanapon1105@yahoo.co.jp>
2024-03-26 03:12:31 +0900

    rootページと活動報告ページを分離

---
+++ pages/announcement/index.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { client } from "@/libs/client";
+import { AnnouncementItem } from "@/components/AnnouncementItem";
+import Link from "next/link";
+
+type Data = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+  category: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    name: string;
+  };
+  OGP: {
+    url: string;
+    height: number;
+    width: number;
+  } | null;
+};
+
+const Announcement = () => {
+  const [data, setData] = useState<Data[] | null>(null);
+  // propsからデータを使用
+  useEffect(() => {
+    async function fetchData() {
+      const response = await client.get({ endpoint: "news" });
+      console.log(response.contents);
+      setData(response.contents);
+    }
+
+    fetchData();
+  }, []);
+  if (!data)
+    return (
+      <>
+        <div
+          style={{
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <p>loading...</p>
+        </div>
+      </>
+    );
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="header">
+          <p>活動報告</p>
+          <Link href=".." style={{ color: "black", textDecoration: "none" }}>
+            ×
+          </Link>
+        </div>
+        <div style={{ paddingTop: "10vh" }}>
+          {(() => {
+            const res = [];
+            for (const elem of data) {
+              res.push(<AnnouncementItem key={elem.id} elem={elem} />);
+            }
+            return res;
+          })()}
+        </div>
+      </div>
+      <style jsx>{`
+        .main {
+        }
+        .container {
+          width: 60vw;
+          margin: 0 20vw;
+          display: flex;
+          flex-direction: column;
+          position: fixed;
+          z-index: 1;
+          height: 85vh;
+          border-bottom: 1px solid #ccc;
+          overflow: scroll;
+        }
+
+        .container::-webkit-scrollbar {
+          display: none;
+        }
+
+        .header {
+          z-index: 10;
+          position: fixed;
+          width: 60vw;
+          font-size: 1.5rem;
+          font-weight: 100;
+          display: flex;
+          justify-content: space-between;
+          height: 6rem;
+          line-height: 6rem;
+          border-bottom: 1px solid #ccc;
+          background-color: #fafafa;
+        }
+
+        @media screen and (max-width: 600px) {
+          .header {
+            width: 94vw;
+          }
+          .container {
+            width: 94vw;
+            margin: 0 3vw;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Announcement;
+
+// getStaticProps関数。ビルド時に実行される
+export async function getStaticProps() {
+  // データの取得
+  const data = await client.get({
+    endpoint: "news",
+  });
+
+  // 取得したデータをpropsとしてページコンポーネントに渡す
+  return {
+    props: { data }, // 注意: 実際にこのデータ構造がclient.getから返されるかはAPIの設計に依存します
+  };
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,91 +1,89 @@
 import styles from "@/styles/Home.module.css";
-import { useEffect, useState } from "react";
-import { client } from "@/libs/client";
-import { AnnouncementItem } from "@/components/AnnouncementItem";
-
-type Data = {
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  revisedAt: string;
-  title: string;
-  content: string;
-  category: {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    revisedAt: string;
-    name: string;
-  };
-  OGP: {
-    url: string;
-    height: number;
-    width: number;
-  } | null;
-};
+// import { Announcement } from "@/components/Announcement";
+import Image from "next/image";
+import Link from "next/link";
 
 // Homeコンポーネント。props経由でデータを受け取る
 const Home = () => {
-  const [data, setData] = useState<Data[] | null>(null);
-  // propsからデータを使用
-  useEffect(() => {
-    async function fetchData() {
-      const response = await client.get({ endpoint: "news" });
-      console.log(response.contents);
-      setData(response.contents);
-    }
-
-    fetchData();
-  }, []);
-  if (!data)
-    return (
-      <>
-        <div
-          style={{
-            width: "100vw",
-            height: "100vh",
-            textAlign: "center",
-            lineHeight: "100vh",
-          }}
-        >
-          <p>loading...</p>
-        </div>
-      </>
-    );
   return (
     <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <p>お知らせ</p>
-          <p>×</p>
-        </div>
-        <div style={{ paddingTop: "3rem" }}>
-          {(() => {
-            const res = [];
-            for (const elem of data) {
-              res.push(<AnnouncementItem key={elem.id} elem={elem} />);
-            }
-            return res;
-          })()}
-        </div>
+      <div className="logo">
+        <Image
+          src="/logo.svg"
+          fill
+          priority
+          style={{
+            objectFit: "contain",
+          }}
+          alt="第３の家族"
+        />
       </div>
+      <div className="link">
+        <Link
+          href="/announcement"
+          style={{
+            textDecoration: "none",
+            color: "black",
+            borderLeft: "1px solid #ccc",
+            marginBottom: "1rem",
+            paddingLeft: "1rem",
+          }}
+        >
+          活動報告
+        </Link>
+        <Link
+          href="#"
+          style={{
+            textDecoration: "none",
+            color: "black",
+            borderLeft: "1px solid #ccc",
+            marginBottom: "1rem",
+            paddingLeft: "1rem",
+          }}
+        >
+          クリエイティブ事業
+        </Link>
+        <Link
+          href="#"
+          style={{
+            textDecoration: "none",
+            color: "black",
+            borderLeft: "1px solid #ccc",
+            marginBottom: "1rem",
+            paddingLeft: "1rem",
+          }}
+        >
+          効果
+        </Link>
+      </div>
+
+      <style jsx>
+        {`
+          .logo {
+            width: 50%;
+            height: 200px;
+            margin-bottom: 2rem;
+            position: relative;
+          }
+          .link {
+            text-decoration: none;
+            color: black;
+            font-weight: 100;
+            font-size: 1.2rem;
+            display: flex;
+            flex-direction: column;
+          }
+
+          @media screen and (max-width: 600px) {
+            .logo {
+              margin-top: 20vh;
+              width: 70%;
+            }
+          }
+        `}
+      </style>
     </main>
   );
 };
-
-// getStaticProps関数。ビルド時に実行される
-export async function getStaticProps() {
-  // データの取得
-  const data = await client.get({
-    endpoint: "news",
-  });
-
-  // 取得したデータをpropsとしてページコンポーネントに渡す
-  return {
-    props: { data }, // 注意: 実際にこのデータ構造がclient.getから返されるかはAPIの設計に依存します
-  };
-}
 
 export default Home;
