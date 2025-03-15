88aca6660c3698e10cd2e5899dc7584ff77eb1f5
k1105 <kanapon1105@yahoo.co.jp>
2025-03-15 10:19:42 +0900

    スタイルファイルを別離

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -1,13 +1,13 @@
 // components/NavButton.js
-import { useEffect, useRef, useState } from "react";
-import { useRouter } from "next/router";
+import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
 import Link from "next/link";
-import classes from "@/styles/HamburgerMenu.module.scss";
-import { Logo } from "./Logo";
-import { ExternalLinkIcon } from "./icons/ExternalLinkIcon";
-import { SocialFacebook } from "./icons/SocialFacebook";
-import { SocialX } from "./icons/SocialX";
-import { SocialInstagram } from "./icons/SocialInstagram";
+import styles from "@/styles/HamburgerMenu.module.scss";
+import {Logo} from "./Logo";
+import {ExternalLinkIcon} from "./icons/ExternalLinkIcon";
+import {SocialFacebook} from "./icons/SocialFacebook";
+import {SocialX} from "./icons/SocialX";
+import {SocialInstagram} from "./icons/SocialInstagram";
 
 export default function HamburgerMenu() {
   const [isVisible, setIsVisible] = useState(false);
@@ -24,27 +24,29 @@ export default function HamburgerMenu() {
     <>
       <div
         onClick={() => setIsVisible(!isVisible)}
-        className="button-container"
+        className={styles.buttonContainer}
       >
-        <div className={`menu-button ${isVisible && "open"}`} />
-        <p>Menu</p>
+        <div className={`${styles.menuButton} ${isVisible && styles.open}`} />
+        <p className={styles.menuTitle}>Menu</p>
       </div>
-      <div className={`document-overlay ${isVisible && "active"}`} />
-      <div className={`nav-overlay ${isVisible && "visible"}`}>
-        <div className="view-wrapper">
-          <div className="first-view">
-            <nav className={classes.nav}>
-              <div className={classes.logo}>
+      <div
+        className={`${styles.documentOverlay} ${isVisible && styles.active}`}
+      />
+      <div className={`${styles.navOverlay} ${isVisible && styles.visible}`}>
+        <div className={styles.viewWrapper}>
+          <div className={styles.firstView}>
+            <nav className={styles.nav}>
+              <div className={styles.logo}>
                 <Link href="/">
                   <Logo />
                 </Link>
               </div>
-              <ul className={classes.navLink}>
-                <li className={classes.list}>
+              <ul className={styles.navLink}>
+                <li className={styles.list}>
                   <Link
                     href="/about"
-                    className={`${classes.link} ${
-                      router.pathname === "/about" ? classes.active : ""
+                    className={`${styles.link} ${
+                      router.pathname === "/about" ? styles.active : ""
                     }`}
                     onClick={() => {
                       setIsVisible(!isVisible);
@@ -53,11 +55,11 @@ export default function HamburgerMenu() {
                     第３の家族とは
                   </Link>
                 </li>
-                <li className={classes.list}>
+                <li className={styles.list}>
                   <Link
                     href="/service"
-                    className={`${classes.link} ${
-                      router.pathname === "/service" ? classes.active : ""
+                    className={`${styles.link} ${
+                      router.pathname === "/service" ? styles.active : ""
                     }`}
                     onClick={() => {
                       setIsVisible(!isVisible);
@@ -66,11 +68,11 @@ export default function HamburgerMenu() {
                     事業内容
                   </Link>
                 </li>
-                <li className={classes.list}>
+                <li className={styles.list}>
                   <Link
                     href="/announcement"
-                    className={`${classes.link} ${
-                      router.pathname === "/announcement" ? classes.active : ""
+                    className={`${styles.link} ${
+                      router.pathname === "/announcement" ? styles.active : ""
                     }`}
                     onClick={() => {
                       setIsVisible(!isVisible);
@@ -79,21 +81,21 @@ export default function HamburgerMenu() {
                     お知らせ
                   </Link>
                 </li>
-                <li className={classes.list}>
+                <li className={styles.list}>
                   {/* <Link
               href="/report"
-              className={`${classes.link} ${
-                router.pathname === "/report" ? classes.active : ""
+              className={`${styles.link} ${
+                router.pathname === "/report" ? styles.active : ""
               }`}
             >
               レポート
             </Link> */}
                 </li>
-                <li className={classes.list}>
+                <li className={styles.list}>
                   <Link
                     href="/data"
-                    className={`${classes.link} ${
-                      router.pathname === "/data" ? classes.active : ""
+                    className={`${styles.link} ${
+                      router.pathname === "/data" ? styles.active : ""
                     }`}
                     onClick={() => {
                       setIsVisible(!isVisible);
@@ -102,11 +104,11 @@ export default function HamburgerMenu() {
                     データ
                   </Link>
                 </li>
-                <li className={classes.list}>
+                <li className={styles.list}>
                   <Link
                     href="/contact"
-                    className={`${classes.link} ${
-                      router.pathname === "/contact" ? classes.active : ""
+                    className={`${styles.link} ${
+                      router.pathname === "/contact" ? styles.active : ""
                     }`}
                     onClick={() => {
                       setIsVisible(!isVisible);
@@ -116,10 +118,10 @@ export default function HamburgerMenu() {
                   </Link>
                 </li>
               </ul>
-              <div className={classes.navBottom}>
+              <div className={styles.navBottom}>
                 <Link
                   href="/donation"
-                  className={`${classes.link} ${classes.externalLink}`}
+                  className={`${styles.link} ${styles.externalLink}`}
                   onClick={() => {
                     setIsVisible(!isVisible);
                   }}
@@ -128,7 +130,7 @@ export default function HamburgerMenu() {
                 </Link>
                 <Link
                   href="/magazine"
-                  className={`${classes.link} ${classes.externalLink}`}
+                  className={`${styles.link} ${styles.externalLink}`}
                   onClick={() => {
                     setIsVisible(!isVisible);
                   }}
@@ -137,7 +139,7 @@ export default function HamburgerMenu() {
                 </Link>
                 <Link
                   href="https://daisan-ticket.studio.site/"
-                  className={`${classes.link} ${classes.externalLink}`}
+                  className={`${styles.link} ${styles.externalLink}`}
                 >
                   <p>第３の家族に協力する</p>
                   <ExternalLinkIcon
@@ -150,7 +152,7 @@ export default function HamburgerMenu() {
                 </Link>
                 <Link
                   href="https://daisan-kazoku.net"
-                  className={`${classes.link} ${classes.externalLink}`}
+                  className={`${styles.link} ${styles.externalLink}`}
                 >
                   <p>少年少女はこちら</p>
                   <ExternalLinkIcon
@@ -162,7 +164,7 @@ export default function HamburgerMenu() {
                   />
                 </Link>
 
-                <div className={classes.socialIcons}>
+                <div className={styles.socialIcons}>
                   <Link href="https://x.com/daisan_kazoku">
                     <SocialX />
                   </Link>
@@ -178,83 +180,6 @@ export default function HamburgerMenu() {
           </div>
         </div>
       </div>
-      <style jsx>{`
-        .button-container {
-          z-index: 10;
-          display: flex;
-          align-items: center;
-          gap: 1rem;
-          p {
-            line-height: 1rem;
-          }
-        }
-
-        .menu-button {
-          width: 1rem;
-          height: 1rem;
-          border-radius: 50%;
-          background: var(--dark-gray);
-          mix-blend-mode: difference;
-          cursor: pointer;
-          transition: background-color 300ms ease-in;
-        }
-
-        .menu-button.open {
-          background-color: #ddd;
-        }
-
-        .document-overlay {
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 100vw;
-          height: 100vh;
-          background: black;
-          z-index: 0;
-          opacity: 0;
-          pointer-events: none;
-          transition: all 1s ease;
-        }
-
-        .document-overlay.active {
-          opacity: 0.3;
-        }
-
-        .nav-overlay {
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 100vw;
-          height: 100vh;
-          background: white;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          z-index: 1;
-          clip-path: circle(0% at calc(97vw - 20px) 55px);
-          transition: clip-path 1s ease-in-out;
-        }
-
-        .nav-overlay.visible {
-          clip-path: circle(200% at calc(97vw - 20px) 55px);
-        }
-
-        .view-wrapper {
-          width: 100vw;
-          height: 100vh;
-        }
-
-        .first-view {
-          margin: 35vh 10vw 0;
-        }
-
-        @media screen and (max-width: 600px) {
-          .first-view {
-            width: 94vw;
-            margin: 5vh 3vw;
-          }
-        }
-      `}</style>
     </>
   );
 }
